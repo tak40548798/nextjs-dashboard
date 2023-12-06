bc81494653b57a32a9813bfc56254821fffb0aa5
tak40548798 <tak40548798@gmail.com>
2023-12-06 17:03:24 +0800

    fix build bug

--- auth.ts
+++ auth.ts
@@ -28,7 +28,7 @@ export const { auth, signIn, signOut } = NextAuth({
 
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
-          const user = await getUser(email) as any;
+          const user = await getUser(email);
           const passwordsMatch = await bcrypt.compare(password, user.password);
 
           if (passwordsMatch) return user;

29d1576def426607e625572800fd02f4e7ca3022
tak40548798 <tak40548798@gmail.com>
2023-12-06 14:32:58 +0800

    chapter15 final

---
+++ auth.config.ts
@@ -0,0 +1,24 @@
+import type { NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        if (isLoggedIn) {
+          return true; // 把沒有認證的使用整重新定向到 login page
+        }
+        return false;
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [Credentials({})],
+} satisfies NextAuthConfig;
